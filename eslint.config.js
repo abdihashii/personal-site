194adc31472922afb0daec5bcbf6f9c774c97eea
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2025-12-07 00:00:52 -0600

    chore: update ESLint configuration and dependencies, disable default formatter in VSCode settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,43 @@
-//  @ts-check
+import antfu from '@antfu/eslint-config';
 
-import { tanstackConfig } from '@tanstack/eslint-config'
-
-export default [...tanstackConfig]
+export default antfu({
+  type: 'app',
+  react: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: 'single',
+  },
+  ignores: [
+    '**/*.gen.ts',
+    'node_modules',
+    'pnpm-lock.yaml',
+    'dist',
+    'build',
+    'public',
+    'public/**',
+    'src/assets',
+    'src/assets/**',
+  ],
+}, {
+  rules: {
+    'ts/no-redeclare': 'off',
+    'ts/consistent-type-definitions': ['error', 'interface'],
+    'no-console': ['warn'],
+    'antfu/no-top-level-await': ['off'],
+    'node/prefer-global/process': ['off'],
+    'node/no-process-env': ['error'],
+    'perfectionist/sort-imports': ['error', {
+      tsconfigRootDir: '.',
+    }],
+    'unicorn/filename-case': ['error', {
+      case: 'kebabCase',
+      ignore: ['README.md', 'CLAUDE.md'],
+    }],
+    'style/brace-style': ['error', '1tbs', {
+      allowSingleLine: true,
+    }],
+    'style/arrow-parens': ['error', 'always'],
+    'antfu/if-newline': ['off'],
+  },
+});
